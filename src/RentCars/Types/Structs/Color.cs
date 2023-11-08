5b10a788b8442705b8a2f0b4b93cf1acb717505c
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-11-08 10:34:11 -0300

    Requisito 1

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -3,5 +3,12 @@ namespace RentCars.Types;
 //1 - Crie uma `Struct` para as **Cores**
 public struct Color
 {
+    public String Name { get; set; }
+    public String Hex { get; set; }
 
+    public Color(String name, String hex)
+    {
+        Name = name;
+        Hex = hex;
+    }
 }
