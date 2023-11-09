b9d3c9ae08a29408f1c5ea9addb7efb616207220
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-11-09 09:59:52 -0300

    req 10 e 11

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -8,23 +8,32 @@ public class Rent
     public Person Person { get; set; }
     public int DaysRented { get; set; }
     public double Price { get; set; }
-    public RentStatus Status { get; set; }
+    public RentStatus Status { get; set; } = RentStatus.Confirmed;
 
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+        Price = person is PhysicalPerson ? vehicle.PricePerDay * daysRented
+            : vehicle.PricePerDay * daysRented * 0.9;
+        vehicle.IsRented = true;
+        person.Debit = Price;
     }
+   
+
+    
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Cancel()
     {
-        throw new NotImplementedException();
-    }
+        Status = RentStatus.Canceled;
+}
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Finish()
     {
-        throw new NotImplementedException();
-    }
+        Status = RentStatus.Finished;
+}
 }
